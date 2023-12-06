32499c0ebfe225ef2d8ca4a288db93e5abeb134e
Krzyś <dyziob65@gmail.com>
2023-12-06 23:19:38 +0100

    working adding products to basket

--- src/main/java/com/gigatech/shop/controller/HomeController.java
+++ src/main/java/com/gigatech/shop/controller/HomeController.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -23,6 +26,24 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model, HttpSession httpSession){
+        model.addAttribute("items", itemRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/add/{itemId}")
+    public String addItemToCart(@PathVariable("itemId") Long itemId, Model model, HttpSession session) {
+        @SuppressWarnings("unchecked")
+        List<Item> cart = (List<Item>) session.getAttribute("cart");
+        if(cart == null){
+            cart = new ArrayList<>();
+        }
+
+        Optional<Item> oItem = itemRepository.findById(itemId);
+        if (oItem.isPresent()) {
+            Item item = oItem.get();
+            cart.add(item);
+            session.setAttribute("cart",cart);
+        }
         model.addAttribute("items", itemRepository.findAll());
         return "index";
     }
